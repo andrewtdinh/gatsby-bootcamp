d0fa1a1e64e6875b237038d57c692966291fbde7
Andrew Dinh <andrewdinh74@yahoo.com>
2019-12-25 18:48:23 -0800

    Add a contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <p>
+        You can contact me on LinkedIn 
+        <a>@LinkedIn</a>
+        or
+      </p>
+    </div>
+  )
+}
+
+export default ContactPage;
