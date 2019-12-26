9fc5c6eea65796e40d960bb57c0a3adbf1a22afc
Andrew Dinh <andrewdinh74@yahoo.com>
2019-12-25 19:43:09 -0800

    Fix the About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const AboutPage = () => {
   return (
     <div>
-      <h1>About Me</h1>
-      <p>I am Andrew Dinh, a frontend dev</p>
+      <h1>Hello</h1>
+      <h2>I am Andrew Dinh</h2>
+      <p>
+        I am a frontend React developer living in the beautiful Bay Area, California. <br/>
+        Need a developer?  <Link to="/contact">Contact me.</Link>
+
+      </p>
     </div>
   )
 }
