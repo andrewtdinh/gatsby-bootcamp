b29dd3417d9ff2d215f9a601d468237f011aceb5
Andrew Dinh <andrewdinh74@yahoo.com>
2019-12-26 22:06:29 -0800

    Export footer and bring it into the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
+import Footer from '../components/footer';
 
 const IndexPage = () => {
   return (
     <div>
       <h1>Hello!</h1>
       <h2>I am Andrew Dinh</h2>
+      <Footer />
     </div>
   )
 }
