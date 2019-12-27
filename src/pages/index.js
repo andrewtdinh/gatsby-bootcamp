674b53608921d7dd87bdb8ab83a48e79dd3d512c
Andrew Dinh <andrewdinh74@yahoo.com>
2019-12-27 10:31:29 -0800

    Bring Layout component into index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,12 @@
 import React from "react"
-import Footer from '../components/footer';
-import Header from '../components/header';
+import Layout from '../components/layout'
 
 const IndexPage = () => {
   return (
-    <div>
-      <Header/>
+    <Layout>
       <h1>Hello!</h1>
       <h2>I am Andrew Dinh</h2>
-      <Footer/>
-    </div>
+    </Layout>
   )
 }
 
