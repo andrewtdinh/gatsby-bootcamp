7a065cd9ea68a86b25cfb2041bdf59ae44338b5a
Andrew Dinh <andrewdinh74@yahoo.com>
2019-12-26 23:06:39 -0800

    Add navigational links to the header

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link }  from 'gatsby';
 
 const Header = () => {
   return (
     <header>
       <h1>The Great Gatsby Bootcamp</h1>
+      <div style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "200px"
+      }}>
+        <Link to="/about">About</Link>
+        <Link to="/blog">Blog</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
     </header>
   )
 }
