87308241332bdc9e4b25dc56dfbb1f437e59991a
Andrew Dinh <andrewdinh74@yahoo.com>
2019-12-27 09:55:01 -0800

    Change the header style

--- src/components/header.js
+++ src/components/header.js
@@ -10,23 +10,7 @@ const Header = () => {
       alignContent: "center"
     }}>
       <h1>Trieu (Andrew) Dinh</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/blog">Blog</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-      {/* <div style={{
+      <nav style={{
         display: "flex",
         flexDirection: "row",
         justifyContent: "space-between",
@@ -34,10 +18,11 @@ const Header = () => {
         width: "200px",
         height: "35px"
       }}>
+        <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/blog">Blog</Link>
         <Link to="/contact">Contact</Link>
-      </div> */}
+      </nav>
     </header>
   )
 }
