6b78be33d53fa8f3fc56f53ff7db77420bb664ec
Andrew Dinh <andrewdinh74@yahoo.com>
2019-12-27 10:33:12 -0800

    Bring Layout component into about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import Footer from '../components/footer'
-import Header from '../components/header';
+import Layout from '../components/layout'
 
 const AboutPage = () => {
   return (
-    <div>
-      <Header/>
+    <Layout>
       <h2>Hello, I am Andrew Dinh</h2>
       <p>
         I am a frontend React developer living in the beautiful Bay Area, California. <br/>
         Need a developer?  <Link to="/contact">Contact me.</Link>
       </p>
-      <Footer/>
-    </div>
+    </Layout>
   )
 }
 
