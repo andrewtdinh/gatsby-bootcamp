eeaef91587e16fa0bd3da2bf80030a37333385f3
Andrew Dinh <andrewdinh74@yahoo.com>
2019-12-30 12:46:11 -0800

    Create layout styles and apply it to the layout container

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 import Header from './header'
 import Footer from './footer'
-import '../styles/index.scss'
+import layoutStyles from './layout.module.scss'
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className={layoutStyles.container}>
       <Header />
       {children}
       <Footer />
