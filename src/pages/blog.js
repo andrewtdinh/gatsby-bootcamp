3e99118d60e3e20968a448e2d9efd0890c970628
Andrew Dinh <andrewdinh74@yahoo.com>
2020-01-02 14:20:56 -0800

    Set up static query to look for all markdown posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import Layout from '../components/layout'
+import { useStaticQuery, graphql } from 'gatsby'
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            excerpt
+            html
+          }
+        }
+      }
+    }
+  `)
   return (
     <Layout>
       <h1>Blog</h1>
