65a670e5cda007ca126ec9c80620be091d388a79
Andrew Dinh <andrewdinh74@yahoo.com>
2020-01-03 16:19:53 -0800

    Add link from the list of blogs to each blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout'
-import { useStaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql, Link } from 'gatsby'
 
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
@@ -12,6 +12,9 @@ const BlogPage = () => {
               title
               date
             }
+            fields {
+              slug
+            }
           }
         }
       }
@@ -22,9 +25,12 @@ const BlogPage = () => {
       <h1>Blog</h1>
       <ol>
         {data.allMarkdownRemark.edges.map((edge) => {
+          const pathToBlog = `/blog/${edge.node.fields.slug}`
           return (
             <li>
-              <h2>{edge.node.frontmatter.title}</h2>
+              <Link to={pathToBlog}>
+                <h2>{edge.node.frontmatter.title}</h2>
+              </Link>
               <p>{edge.node.frontmatter.date}</p>
             </li>
           )
