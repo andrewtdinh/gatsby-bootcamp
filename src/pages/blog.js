5e02ef61b267c7e7a7ea06d11dfc03e497e59c25
Andrew Dinh <andrewdinh74@yahoo.com>
2020-01-03 16:39:05 -0800

    Refactor to eliminate unnecessary variable declaration

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -25,10 +25,9 @@ const BlogPage = () => {
       <h1>Blog</h1>
       <ol>
         {data.allMarkdownRemark.edges.map((edge) => {
-          const pathToBlog = `/blog/${edge.node.fields.slug}`
           return (
             <li>
-              <Link to={pathToBlog}>
+              <Link to={`/blog/${edge.node.fields.slug}`}>
                 <h2>{edge.node.frontmatter.title}</h2>
               </Link>
               <p>{edge.node.frontmatter.date}</p>
