27885f94351ddcbfbad4255f04d031e8cbedd5d1
Andrew Dinh <andrewdinh74@yahoo.com>
2020-01-04 17:46:35 -0800

    Apply blog posts style to the blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Layout from '../components/layout'
 import { useStaticQuery, graphql, Link } from 'gatsby'
 
+import blogStyles from './blog.module.scss'
+
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
     query {
@@ -23,7 +25,7 @@ const BlogPage = () => {
   return (
     <Layout>
       <h1>Blog</h1>
-      <ol>
+      <ol className={blogStyles.posts}>
         {data.allMarkdownRemark.edges.map((edge) => {
           return (
             <li>
