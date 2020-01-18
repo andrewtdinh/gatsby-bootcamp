ee25c52c3c00b4b7a98a8403efdd0fcd4e20e3af
Andrew Dinh <andrewdinh74@yahoo.com>
2020-01-17 19:50:17 -0800

    Add a bare 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import LayOut from '../components/layout';
+import Layout from '../components/layout';
+
+const NotFound = () => {
+  return (
+    <Layout>
+      <h1>Page not found</h1>
+    </Layout>
+  )
+}
+
+export default NotFound;
