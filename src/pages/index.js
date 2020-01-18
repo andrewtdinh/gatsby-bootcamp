6bd68e03dd33dcc555d1ada4bb7f476b031263cf
Andrew Dinh <andrewdinh74@yahoo.com>
2020-01-17 20:29:29 -0800

    Fix import error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Layout from '../components/layout'
+import Head from '../components/head'
 
 const IndexPage = () => {
   return (
     <Layout>
+      <Head />
       <h1>Hello!</h1>
       <h2>I am Andrew Dinh</h2>
     </Layout>
